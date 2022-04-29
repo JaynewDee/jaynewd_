7826310bb533638edbfb3776daf32eb1948dafda
NuminousBlue <jdiehl2236@gmail.com>
2022-04-29 18:43:58 -0500

    mongoose config

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const uRi = process.env.MONGODB_URI;
 
-mongoose.connect( uRi || 'mongodb://localhost:3001', {
-});
-console.log(process.env.MONGODB_URI)
+mongoose.connect( 'mongodb+srv://jaynewdee:!2babytiger2!@cluster0.fwmsg.mongodb.net/synthetic?retryWrites=true&w=majority')
+
 module.exports = mongoose.connection;
