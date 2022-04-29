ba9b917b82c7167aa8a7571d840d30bd962fd186
NuminousBlue <jdiehl2236@gmail.com>
2022-04-29 18:34:22 -0500

    repairing statics

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const uRi = process.env.MONGODB_URI;
 
-mongoose.connect( uRi || 'mongodb://localhost/googlebooks', {
+mongoose.connect( uRi || 'mongodb://localhost:3001', {
 });
 console.log(process.env.MONGODB_URI)
 module.exports = mongoose.connection;
