03b8abc4b7aca8fa64574f09cbfd725fa1fbb72e
NuminousBlue <jdiehl2236@gmail.com>
2023-01-12 03:01:32 -0600

    branching to session feature

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose");
+const { connect, connection } = require("mongoose");
 const {} = require("dotenv/config");
 
-const URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/synthetic";
-console.log(process.env.MONGODB_URI);
-connect(URI, {
+const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/synthetic";
+
+connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  autoCreate: true
+  useUnifiedTopology: true
 });
 
-module.exports = mongoose.connection;
+module.exports = connection;
